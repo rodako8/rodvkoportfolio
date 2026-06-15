55846b071f64dcfb25ef83a547abc71f8a44b074
rodako8 <hc.shin006217@gmail.com>
2026-06-15 15:37:25 +0900

    Add files via upload

---
+++ js/gallery-data.js
@@ -0,0 +1,8 @@
+const galleryData = [
+  {id:1,title:"Grapic Design",category:"2D",categoryName:"2D",date:"2026.06.01",image:"images/gallery/photo01.jpg",desc:"Works using Adobe Photoshop, Illustration, etc."},
+  {id:2,title:"3D Modeling",category:"3D",categoryName:"3D",date:"2026.06.02",image:"images/gallery/photo02.jpg",desc:"3d modeling works using rhino, 3dsmax, etc."},
+  {id:3,title:"Video",category:"Video",categoryName:"Video",date:"2026.06.03",image:"images/gallery/photo03.jpg",desc:"Video works using Premier Pro, After Effects, etc."},
+  {id:4,title:"Package",category:"2D",categoryName:"2D",date:"2026.06.04",image:"images/gallery/photo04.jpg",desc:"User-friendly package design."},
+  {id:5,title:"Poster Design",category:"2D",categoryName:"2D",date:"2026.06.05",image:"images/gallery/photo05.jpg",desc:"A variety of poster designs."},
+  {id:6,title:"CI / BI",category:"Addition",categoryName:"Addition",date:"2026.06.06",image:"images/gallery/photo06.jpg",desc:"Building a personal brand,."}
+];
